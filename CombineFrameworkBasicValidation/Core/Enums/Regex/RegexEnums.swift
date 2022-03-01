ecede99b180e1ac3f5aca5427a1f858854e9ced0
alicanyilmazz <alicanyilmaz101@gmail.com>
2022-03-01 13:34:29 +0300

    Regex refactored.

--- CombineFrameworkBasicValidation/Core/Enums/Regex/RegexEnums.swift
+++ CombineFrameworkBasicValidation/Core/Enums/Regex/RegexEnums.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 enum RegexPatterns{
-    static let email = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    static let email = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{3,64}"
     static let between3to20 = "^.{3,20}$"
     static let name = "^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$"
     static let between3to15 = "^.{3,15}$"
