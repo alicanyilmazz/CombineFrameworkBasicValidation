b8401906d11e67ce34953b6e53e9cdf696744c63
alicanyilmazz <alicanyilmaz101@gmail.com>
2022-03-01 12:53:23 +0300

    Field variables added to FormTextCollection.

--- CombineFrameworkBasicValidation/Core/Base/FormTextCollection.swift
+++ CombineFrameworkBasicValidation/Core/Base/FormTextCollection.swift
@@ -14,12 +14,20 @@ class FormTextCollection{
     private(set) var subject = PassthroughSubject<(String,Int),Never>()
     private var subscriptions = Set<AnyCancellable>()
     
+    private var textField : UITextField
+    private var label : UILabel
+    private var imageView : UIImageView
+    
+    init(textField : UITextField , label : UILabel , imageView : UIImageView){
+        self.textField = textField
+        self.label = label
+        self.imageView = imageView
+    }
 }
 
 extension FormTextCollection{
     
-    
-     func setup(formComponent : FormTextComponent , textField : UITextField , label : UILabel, imageView : UIImageView){
+     func setup(formComponent : FormTextComponent){
        
         NotificationCenter.default.publisher(for: UITextField.textDidChangeNotification, object: textField).compactMap {
             ($0.object as? UITextField)?.text}.map(String.init).sink{ [weak self] val in
@@ -32,21 +40,21 @@ extension FormTextCollection{
                         try validator.validate(val)
                     }
                     
-                    textField.valid()
-                    label.text = "Everything seems ok."
-                    label.textColor = .systemGreen
-                    imageView.image = UIImage(systemName: "checkmark.circle")
-                    imageView.tintColor = .systemGreen
+                    self.textField.valid()
+                    self.label.text = "Everything seems ok."
+                    self.label.textColor = .systemGreen
+                    self.imageView.image = UIImage(systemName: "checkmark.circle")
+                    self.imageView.tintColor = .systemGreen
                     
                 }catch{
-                    textField.invalid()
+                    self.textField.invalid()
                     if let validationError = error as? ValidationError{
                         switch validationError {
                         case .custom(let message):
-                            label.text = message
-                            label.textColor = .systemPink
-                            imageView.image = UIImage(systemName: "xmark.circle")
-                            imageView.tintColor = .systemPink
+                            self.label.text = message
+                            self.label.textColor = .systemPink
+                            self.imageView.image = UIImage(systemName: "xmark.circle")
+                            self.imageView.tintColor = .systemPink
                         }
                     }
                 }
@@ -55,3 +63,44 @@ extension FormTextCollection{
     }
 }
 
+/*
+ 
+ extension FormTextCollection{
+     
+      func setup(formComponent : FormTextComponent , textField : UITextField , label : UILabel, imageView : UIImageView){
+        
+         NotificationCenter.default.publisher(for: UITextField.textDidChangeNotification, object: textField).compactMap {
+             ($0.object as? UITextField)?.text}.map(String.init).sink{ [weak self] val in
+                 guard let self = self else { return }
+                 
+                 self.subject.send((val,formComponent.index))
+                 
+                 do{
+                     for validator in formComponent.validations{
+                         try validator.validate(val)
+                     }
+                     
+                     textField.valid()
+                     label.text = "Everything seems ok."
+                     label.textColor = .systemGreen
+                     imageView.image = UIImage(systemName: "checkmark.circle")
+                     imageView.tintColor = .systemGreen
+                     
+                 }catch{
+                     textField.invalid()
+                     if let validationError = error as? ValidationError{
+                         switch validationError {
+                         case .custom(let message):
+                             label.text = message
+                             label.textColor = .systemPink
+                             imageView.image = UIImage(systemName: "xmark.circle")
+                             imageView.tintColor = .systemPink
+                         }
+                     }
+                 }
+                 
+             }.store(in: &subscriptions)
+     }
+ }
+ 
+ */
